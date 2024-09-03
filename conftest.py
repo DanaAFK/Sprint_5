9e9d5e5a55d55ab57e0802487a011782445aaf72
Dana <chertilas@mail.ru>
2024-09-03 14:58:33 +0300

    Realized Test_login_and_order and Redister person fixture

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+@pytest.fixture(scope="function")
+def register_user():
+    driver = webdriver.Chrome()
+    driver.get('https://stellarburgers.nomoreparties.site/')
+
+    account_button = driver.find_element(By.CSS_SELECTOR, "a[href='/account']")
+    account_button.click()
+
+    register_link = driver.find_element(By.CSS_SELECTOR, "a[href='/register']")
+    register_link.click()
+
+    name = driver.find_element(By.XPATH, "//label[text()='Имя']/following-sibling::input[@type='text']")
+    name.send_keys("Дана7")
+
+    emaill = driver.find_element(By.XPATH, "//label[text()='Email']/following-sibling::input")
+    emaill.send_keys("fairy7@mail.com")
+
+    password = driver.find_element(By.XPATH, "//label[text()='Пароль']/following-sibling::input[@type='password']")
+    password.send_keys("ILoveMinions098")
+
+    register_button = driver.find_element(By.XPATH, "//button[text()='Зарегистрироваться']")
+    register_button.click()
+
+    WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.XPATH, "//h2[text()='Вход']"))
+    )
+
+    yield driver
+    driver.quit()
