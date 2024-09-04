ff856b43b44cf25f966bb547d9815bfdbd203efb
Dana <chertilas@mail.ru>
2024-09-04 10:08:09 +0300

    Realized test_constructor_button sucsessfull and new fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,10 +17,10 @@ def register_user():
     register_link.click()
 
     name = driver.find_element(By.XPATH, "//label[text()='Имя']/following-sibling::input[@type='text']")
-    name.send_keys("Дана97")
+    name.send_keys("Дана96")
 
     emaill = driver.find_element(By.XPATH, "//label[text()='Email']/following-sibling::input")
-    emaill.send_keys("fairy97@mail.com")
+    emaill.send_keys("fairy96@mail.com")
 
     password = driver.find_element(By.XPATH, "//label[text()='Пароль']/following-sibling::input[@type='password']")
     password.send_keys("ILoveMinions098")
@@ -37,13 +37,34 @@ def register_user():
 
 
 @pytest.fixture(scope="function")
-def login_user():
+def register_and_login_user():
     driver = webdriver.Chrome()
     driver.get('https://stellarburgers.nomoreparties.site/')
 
+    account_button = driver.find_element(By.CSS_SELECTOR, "a[href='/account']")
+    account_button.click()
+
+    register_link = driver.find_element(By.CSS_SELECTOR, "a[href='/register']")
+    register_link.click()
+
+    name = driver.find_element(By.XPATH, "//label[text()='Имя']/following-sibling::input[@type='text']")
+    name.send_keys("Дана95")
+
+    emaill = driver.find_element(By.XPATH, "//label[text()='Email']/following-sibling::input")
+    emaill.send_keys("fairy95@mail.com")
+
+    password = driver.find_element(By.XPATH, "//label[text()='Пароль']/following-sibling::input[@type='password']")
+    password.send_keys("ILoveMinions098")
+
+    register_button = driver.find_element(By.XPATH, "//button[text()='Зарегистрироваться']")
+    register_button.click()
+
+    WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.XPATH, "//h2[text()='Вход']"))
+    )
 
     email = driver.find_element(By.XPATH, "//label[text()='Email']/following-sibling::input[@type='text']")
-    email.send_keys("fairy97@mail.com")
+    email.send_keys("fairy95@mail.com")
 
     password = driver.find_element(By.XPATH, "//label[text()='Пароль']/following-sibling::input[@type='password']")
     password.send_keys("ILoveMinions098")
@@ -51,7 +72,7 @@ def login_user():
     login_button = driver.find_element(By.XPATH, "//button[text()='Войти']")
     login_button.click()
 
-    WebDriverWait(driver, 20).until(
+    WebDriverWait(driver, 10).until(
         expected_conditions.presence_of_element_located((By.XPATH, "//button[text()='Оформить заказ']"))
     )
 
--- tests/test_constructor_button.py
+++ tests/test_constructor_button.py
@@ -1,20 +1,23 @@
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+def test_constructor_button(register_and_login_user):
+    driver = register_and_login_user
 
-def test_constructor_button(register_user, login_user):
-    driver = register_user
+    personal_account = driver.find_element(By.XPATH, "//p[text()='Личный Кабинет']")
+    personal_account.click()
 
-    constructor = driver.find_element(By.CSS_SELECTOR, "a[href='/_']")
+    constructor = driver.find_element(By.CSS_SELECTOR, "a[href='/']")
     constructor.click()
 
-    logo = WebDriverWait(driver,2).until(
-        expected_conditions.visibility_of_element_located((By.XPATH,"//h1[text()='Соберите бургер']"))
+    logo = WebDriverWait(driver, 2).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, "//h1[text()='Соберите бургер']"))
     )
 
     assert logo.text == 'Соберите бургер'
+
 
 
 
